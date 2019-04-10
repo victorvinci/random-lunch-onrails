e6e699383cb7e1492e2ad172f2faf2b311f73bc8
victor vinci <victorvinci@protonmail.com>
2019-04-10 18:43:44 +0200

    customize form field and pages titles

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: {message: 'Nome, por favor.'}
+  validates :description, presence: {message: 'Essa parte é importante!'}
+  validates :website, presence: {message: 'Pode ser TripAdvisor também.'}
+  validates :address, presence: {message: 'Onde fica?'}
+  validates :price, presence: {message: 'Quanto se gasta nesse lugar?'}
+  validates :cuisine, presence: {message:'Não se esqueça dessa parte!'}
+  validates :food_type, presence: {message: 'Selecione uma das opções.'}
 end
