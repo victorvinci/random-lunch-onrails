0719cc232cbfd8d95e787f45b7e4b01f54cff9fd
victor vinci <victorvinci@protonmail.com>
2019-04-10 17:58:59 +0200

    add scaffold test for restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2019_04_10_105102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "description"
