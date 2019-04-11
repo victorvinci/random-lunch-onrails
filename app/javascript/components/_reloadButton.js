317ee6f4deff5a610aaa9928ccd2420272707a66
victor vinci <victorvinci@protonmail.com>
2019-04-11 21:26:10 +0200

    minor adjustments on style and add back animation on button click

--- app/javascript/components/_reloadButton.js
+++ app/javascript/components/_reloadButton.js
@@ -2,16 +2,16 @@ function reloadContent() {
   document.getElementById("button").addEventListener("click", function() {
     $("#content-section").fadeOut(1000);
     
-    // setTimeout(function(){
-    //   $("#loader").fadeIn();
-    // }, 800)
+    setTimeout(function(){
+      $("#loader").fadeIn();
+    }, 800)
     
     setTimeout(function() {
       $("#content-container").load(location.href + " #content-container");
 
       setTimeout(function() {
-        // $("#loader").fadeOut();
-        $("#content-section").fadeIn(1000);
+        $("#loader").fadeOut();
+        $("#content-section").fadeIn(500);
       }, 2000);
       
     }, 1000);
