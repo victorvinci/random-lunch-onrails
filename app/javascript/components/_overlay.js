7891be98c8ad17ce0de7d0a9fef979d8abdb5504
victor vinci <victorvinci@protonmail.com>
2019-04-12 14:05:26 +0200

    fix breaking layout on mobile

--- app/javascript/components/_overlay.js
+++ app/javascript/components/_overlay.js
@@ -1,7 +1,9 @@
 function loadPage() {
   $(document).ready(function() {
+    $("#content-section").fadeOut();
     setTimeout(function() {
       $("#overlay").fadeOut(1000);
+      $("#content-section").fadeIn(500);
     }, 5000);
   });
 }
